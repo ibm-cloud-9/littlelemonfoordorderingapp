50448cfd4ab61dd3bf817a20a88af0e6e374240b
Sean Gunter <seanoguntersr@gmail.com>
2024-04-12 06:46:35 -0400

    Uploaded exported wireframe to assets folder

--- utils/utils.js
+++ utils/utils.js
@@ -11,12 +11,9 @@ export function getSectionListData(data) {
     };
      
     if (!Array.isArray(acc[curr.category])) {
-      //console.log(curr.category.charAt(0).toUpperCase() + curr.category.slice(1))
       acc[curr.category] = [menuItem];
-      //acc[curr.category.charAt(0).toUpperCase() + curr.category.slice(1)] = [menuItem]
     } else {
       acc[curr.category].push(menuItem);
-      //acc[curr.category.charAt(0).toUpperCase() + curr.category.slice(1)].push(menuItem)
     }
     return acc;
   }, {});
@@ -28,27 +25,7 @@ export function getSectionListData(data) {
       data: item,
     };
   });
-  console.log('Debug: getSectionListData:', sectionListData)
-  return sectionListData;
-}
 
-
-export function othergetSectionListData(data) {
-  let sectionListData = []
-  data.map((item) => {
-    let obj = sectionListData.find(x => x.name == item.category.charAt(0).toUpperCase() + item.category.slice(1));
-    if (obj) {
-      sectionListData[sectionListData.indexOf(obj)].data.push({id: item.id, name: item.name, price: item.price, description: item.description, image: item.image})
-    } else {
-      sectionListData.push({
-        name: item.category.charAt(0).toUpperCase() + item.category.slice(1),
-        data:[
-          {id: item.id, name: item.name, price: item.price, description: item.description, image: item.image}
-        ]
-      })
-    }});
-
-  console.log('Debug: getSectionListData:', sectionListData)
   return sectionListData;
 }
 
